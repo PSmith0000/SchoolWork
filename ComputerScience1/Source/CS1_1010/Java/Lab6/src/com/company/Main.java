a30fe28acafa999a3207b58548ee2e1b59d577f0
PSmith0000 <40529988+PSmith0000@users.noreply.github.com>
2020-09-09 18:25:12 -0400

    .

---
+++ ComputerScience1/Source/CS1_1010/Java/Lab6/src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(GetLetterGrade(-1));
+    }
+
+
+    private static char GetLetterGrade(double score) {
+        if(score >= 90) {
+            return 'A';
+        }
+        else if(score >= 80 && score <= 89) {
+            return 'B';
+        }
+        else if(score >= 70 && score <= 79) {
+            return 'C';
+        }
+        else if(score >= 65 && score <= 69) {
+            return 'D';
+        }
+        else if(score <= 65) {
+            return 'F';
+        }
+        return ' ';
+    }
+}
